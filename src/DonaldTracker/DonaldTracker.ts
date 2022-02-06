5197a9bc462cb949f7e5ef915a9ed66a672f3bf2
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2022-02-06 14:43:45 -0500

    Donald tracker no longer creates fn news channel

--- src/DonaldTracker/DonaldTracker.ts
+++ src/DonaldTracker/DonaldTracker.ts
@@ -70,18 +70,7 @@ export class DonaldTracker {
             .addField('Banner', `~~${doc.banner}~~\n${this.data.banner}`)
             .setTimestamp(new Date());
 
-
-        const channels = this.client.channels.cache.filter((c: TextChannel) => c.name.includes("fn-news")) as Collection<Snowflake, TextChannel>
-
-        const neo = this.client.guilds.cache.get(newsChannels.neo.guild)
-        if (!neo.channels.cache.find((c: TextChannel) => c.name.includes("fn-news"))) {
-            const mineChannel = <TextChannel>neo.channels.cache.get("685958708383056034")
-            const c = await neo.channels.create("fn-news", { topic: "Fortnite News and Donald Mustard updates", position: mineChannel.parent.children.size + 1, parent: mineChannel.parent })
-            channels.set(c.id, <TextChannel>neo.channels.cache.get(c.id))
-        }
-
-
-        channels.forEach(c => c.send({ embeds: [e] }))
+for (const id of Object.values(newsChannels)) (<TextChannel>this.client.channels.cache.get(id))?.send({ embeds: [e] });
     }
 
 }
