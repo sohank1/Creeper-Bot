0211956fa274340428d562677928950cca254377
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2022-04-04 22:54:13 -0400

    S

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,77 @@ export const TEST_SERVER = "695646961763614740";
 // let music: Music;
 
 try {
+client.on("messageDelete", async (message) => {
+  let logs = await message.guild.fetchAuditLogs({ type: 72 });
+  let entry = logs.entries.first();
+  if (message.deleted) {
+    const channel = client.channels.cache.get("695646963235946549");
+    if (channel) {
+      const deletedMessageEmbed = new MessageEmbed()
+        .setTitle("Deleted Message")
+        .addField("Message", message.content)
+        .addField("Author", `${message.author.tag} (${message.author.id})`)
+        .addField("Deleter (Can be wrong if message deleted by bot.", `${entry.executor}`)
+        .addField("Server", `${message.guild.name} (${message.guild.id})`)
+        .addField("Channel", `${message.channel.name} (${message.channel.id})`)
+        .setThumbnail("https://media.graytvinc.com/images/810*455/Coronavirus52.jpg")
+        .setColor("FFC433")
+        .setTimestamp()
+        .setFooter("Corona Bot by Creeper " + version);
+      channel.send(deletedMessageEmbed);
+    }
+  }
+});
+client.on("messageUpdate", (oldMessage, newMessage) => {
+  const editlogschannel = client.channels.cache.get("698712954362658857");
+  if (oldMessage.content === newMessage.content) {
+    return;
+  }
+  if (editlogschannel) {
+    const editEmbed = new MessageEmbed()
+      .setTitle("Message Edit")
+      .addField("Old Message", oldMessage.content)
+      .addField("New Message", newMessage.content)
+      .addField("Message Edits (Newest to Oldest)", oldMessage.edits)
+      .addField("Message Edits At", oldMessage.editedAt)
+      .addField("Message Edited Timestamp", oldMessage.editedTimestamp)
+      .addField("Author", `${oldMessage.author.tag} (${oldMessage.author.id})`)
+      .addField("Server", `${oldMessage.guild.name} (${oldMessage.guild.id})`)
+      .addField("Channel", `${oldMessage.channel.name} (${oldMessage.channel.id})`)
+      .setThumbnail("https://media.graytvinc.com/images/810*455/Coronavirus52.jpg")
+      .setColor("FFC433")
+      .setTimestamp()
+      .setFooter("Corona Bot by Creeper " + version);
+
+    editlogschannel.send(editEmbed);
+  }
+});
+
+client.on("ready", () => console.log(`${client.user.tag} has logged in.`));
+
+client.on("messageDeleteBulk", (messages) => {
+  const purgedChannel = client.channels.cache.get("720667264738787340");
+  let deletedArray = messages.array();
+  deletedArray.reverse();
+  deletedArray.forEach(async (message) => {
+    if (purgedChannel) {
+      const purgedMessageEmbed = new MessageEmbed()
+        .setTitle(`${deletedArray.length} Purged Messages`)
+        .addField("Message", message.content)
+        .addField("Author", `${message.author.tag} (${message.author.id})`)
+        .addField("Server", `${message.guild.name} (${message.guild.id})`)
+        .addField("Channel", `${message.channel.name} (${message.channel.id})`)
+        .addField("Time Message Was Created", `${message.createdAt.toLocaleString()}`)
+        .addField("Message Edits", `${message.edits}`)
+        .addField("Message Edits Time", `${message.editedAt}`)
+        .setThumbnail("https://media.graytvinc.com/images/1920*1080/Coronavirus52.jpg")
+        .setColor("FFC433")
+        .setTimestamp()
+        .setFooter("Corona Bot by Creeper " + version);
+      await purgedChannel.send(purgedMessageEmbed);
+    }
+  });
+});
   client.on("ready", async () => {
     client.application.commands.fetch().then(console.log);
 
