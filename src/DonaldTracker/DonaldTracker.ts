eaa29dd936f89b33cf23c2f48bb26b5b534ba422
CreeperPlanet26 <51346607+CreeperPlanet26@users.noreply.github.com>
2022-09-06 18:21:58 -0400

    set user agent to stop twitter block

--- src/DonaldTracker/DonaldTracker.ts
+++ src/DonaldTracker/DonaldTracker.ts
@@ -36,6 +36,9 @@ export class DonaldTracker {
 
 
         const page = await (await browser).newPage();
+        await page.setUserAgent(
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+        )
         c.send(`opened page: ${page.url()}`)
 
         page.setDefaultNavigationTimeout(0);
