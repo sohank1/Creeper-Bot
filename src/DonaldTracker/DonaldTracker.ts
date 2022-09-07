8beef6b35bbb7876e5a4ec623b17e68c19254eb4
CreeperPlanet26 <51346607+CreeperPlanet26@users.noreply.github.com>
2022-09-06 20:13:22 -0400

    go back to 5 min fetch rate

--- src/DonaldTracker/DonaldTracker.ts
+++ src/DonaldTracker/DonaldTracker.ts
@@ -93,7 +93,7 @@ export class DonaldTracker {
                 this.saveData();
                 this.sendMessage(doc);
             }
-        }, 10000)  //5 mins 300000
+        }, 300000)  //5 mins 300000
 
     }
 
