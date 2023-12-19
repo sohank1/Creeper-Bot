e6df73cf5a99c751d6c2b3bd635965afa448d840
CreeperPlanet26 <51346607+CreeperPlanet26@users.noreply.github.com>
2023-12-18 21:38:29 -0500

    comment out old instance systems

--- src/postScript.ts
+++ src/postScript.ts
@@ -1,61 +1,61 @@
-import axios from "axios";
+// import axios from "axios";
 
-import { Client, Intents, TextChannel } from "discord.js";
-import express from "express";
+// import { Client, Intents, TextChannel } from "discord.js";
+// import express from "express";
 
-require("dotenv").config();
+// require("dotenv").config();
 
-const client = new Client({ restTimeOffset: 75, intents: new Intents(["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS",]) });
-client.login(process.env.NODE_ENV == 'production' ? process.env.BOT_TOKEN : process.env.DEV_BOT_TOKEN);
+// const client = new Client({ restTimeOffset: 75, intents: new Intents(["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS",]) });
+// client.login(process.env.NODE_ENV == 'production' ? process.env.BOT_TOKEN : process.env.DEV_BOT_TOKEN);
 
-const postScriptSpawnedAt = new Date().toISOString();
-const version = `v${require("../package.json").version}`;
-
-
-const app = express();
-const port = process.env.PORT || 3001;
-app.get("/", (req, res) => res.status(200).json({ msg: 'message from spawned script. old version of the app', postScriptSpawnedAt }));
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
-
-    client.on("ready", () => {
-        const c = (<TextChannel>client.channels.cache.get('1045086199053820004')) || (<TextChannel>client.channels.cache.get("725143127723212830"));
-        c.send("Post script has spawned!!!")
-
-        setInterval(async () => {
-            console.log('this is a test')
-            try {
-                if (process.env.NODE_ENV === "production" && process.env?.HOST_TYPE === "render") {
-                    // console.log(`Fetching the render url from post script. This shouldn't be happening for a long amount of time. This script spawned at: ${postScriptSpawnedAt}`);
-                    // const { data } = await axios.get('https://creeper-bot.onrender.com/');
-                    // console.log(data);
-                    // if (data.serverStartedAt) {
-                    //     c.send(`\`${version}\` there is a serverStartedAt prop. This means new server is ready: \`${data.serverStartedAt}\``)
-                    //     c.send("shutting down the old server that's running the post script...")
-                    //     console.log("shutting down the old server that's running the post script...")
-                    //     process.send("SHUTDOWN_SERVER")
-                    // }
-
-                    // else console.log(`${version} there is not serverStartedAt: ${JSON.stringify(data, null, 2)}`)
-
-                    console.log(`Fetching the render url from post script. This shouldn't be happening for a long amount of time. This script spawned at: ${postScriptSpawnedAt}`);
-                    axios.get('https://creeper-bot.onrender.com/')
-
-                }
-            }
-            catch (e) {
-                console.log(e.message);
-            }
-        }, 600000) // 15 seconds
-
-        const countChannel = <TextChannel>client.channels.cache.get('1045085618683781320')
-        let count = 1;
-        process.env.NODE_ENV === "production" && setInterval(() => {
-            if (count === 1) countChannel.send("**---------------------------------------------**")
-            countChannel.send(`[\`${postScriptSpawnedAt}\`] ---- count: ${count} ---- [\`${version}\`]`)
-            count++
-        }, 10000)
-    })
+// const postScriptSpawnedAt = new Date().toISOString();
+// const version = `v${require("../package.json").version}`;
 
 
-});
+// const app = express();
+// const port = process.env.PORT || 3001;
+// app.get("/", (req, res) => res.status(200).json({ msg: 'message from spawned script. old version of the app', postScriptSpawnedAt }));
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}!`)
+
+//     client.on("ready", () => {
+//         const c = (<TextChannel>client.channels.cache.get('1045086199053820004')) || (<TextChannel>client.channels.cache.get("725143127723212830"));
+//         c.send("Post script has spawned!!!")
+
+//         setInterval(async () => {
+//             console.log('this is a test')
+//             try {
+//                 if (process.env.NODE_ENV === "production" && process.env?.HOST_TYPE === "render") {
+//                     // console.log(`Fetching the render url from post script. This shouldn't be happening for a long amount of time. This script spawned at: ${postScriptSpawnedAt}`);
+//                     // const { data } = await axios.get('https://creeper-bot.onrender.com/');
+//                     // console.log(data);
+//                     // if (data.serverStartedAt) {
+//                     //     c.send(`\`${version}\` there is a serverStartedAt prop. This means new server is ready: \`${data.serverStartedAt}\``)
+//                     //     c.send("shutting down the old server that's running the post script...")
+//                     //     console.log("shutting down the old server that's running the post script...")
+//                     //     process.send("SHUTDOWN_SERVER")
+//                     // }
+
+//                     // else console.log(`${version} there is not serverStartedAt: ${JSON.stringify(data, null, 2)}`)
+
+//                     console.log(`Fetching the render url from post script. This shouldn't be happening for a long amount of time. This script spawned at: ${postScriptSpawnedAt}`);
+//                     axios.get('https://creeper-bot.onrender.com/')
+
+//                 }
+//             }
+//             catch (e) {
+//                 console.log(e.message);
+//             }
+//         }, 600000) // 15 seconds
+
+//         const countChannel = <TextChannel>client.channels.cache.get('1045085618683781320')
+//         let count = 1;
+//         process.env.NODE_ENV === "production" && setInterval(() => {
+//             if (count === 1) countChannel.send("**---------------------------------------------**")
+//             countChannel.send(`[\`${postScriptSpawnedAt}\`] ---- count: ${count} ---- [\`${version}\`]`)
+//             count++
+//         }, 10000)
+//     })
+
+
+// });
--- src/prodStart.ts
+++ src/prodStart.ts
@@ -1,78 +1,78 @@
-import { ChildProcess, fork } from "child_process";
-import { createClient } from "redis";
-import { InstanceManager } from "./InstanceManager";
+// import { ChildProcess, fork } from "child_process";
+// import { createClient } from "redis";
+// import { InstanceManager } from "./InstanceManager";
 
-export const version = `v${require("../package.json").version}`;
+// export const version = `v${require("../package.json").version}`;
 
-(async () => {
-    if (process.env.NODE_ENV !== "production") throw new Error("Only start the app in production");
-    const redis = createClient({ url: process.env.REDIS_URI });
+// (async () => {
+//     if (process.env.NODE_ENV !== "production") throw new Error("Only start the app in production");
+//     const redis = createClient({ url: process.env.REDIS_URI });
 
-    const subscriber = redis.duplicate();
-    await redis.connect()
-    await subscriber.connect();
-    const key = "creeper_bot_prod_server";
+//     const subscriber = redis.duplicate();
+//     await redis.connect()
+//     await subscriber.connect();
+//     const key = "creeper_bot_prod_server";
 
-    let mainProcess: ChildProcess;
-    let postScript: ChildProcess;
+//     let mainProcess: ChildProcess;
+//     let postScript: ChildProcess;
 
-    const instanceManager = new InstanceManager(redis, subscriber, process.env.HOST_TYPE, version, key, {
-        onKeep: () => {
-            if (!postScript) return;
-            console.log("Killing postScript");
-            postScript.kill();
-            postScript = null;
+//     const instanceManager = new InstanceManager(redis, subscriber, process.env.HOST_TYPE, version, key, {
+//         onKeep: () => {
+//             if (!postScript) return;
+//             console.log("Killing postScript");
+//             postScript.kill();
+//             postScript = null;
 
-            mainProcess = fork("dist/index.js");
-            instanceManager.setStatus("online");
+//             mainProcess = fork("dist/index.js");
+//             instanceManager.setStatus("online");
 
-            // mainProcess.on("message", onMainProcessMessage);
-            // mainProcess.on("spawn", onMainProcessMessage);
-        },
-        onShutdown: () => {
-            console.log(`THE MAIN PROCESS IS BEING KILLED!! ${instanceManager._instance} (${mainProcess.pid})`)
-            if (!mainProcess) return;
-            console.log("Killing mainProcess");
-            mainProcess.kill();
-            mainProcess = null;
+//             // mainProcess.on("message", onMainProcessMessage);
+//             // mainProcess.on("spawn", onMainProcessMessage);
+//         },
+//         onShutdown: () => {
+//             console.log(`THE MAIN PROCESS IS BEING KILLED!! ${instanceManager._instance} (${mainProcess.pid})`)
+//             if (!mainProcess) return;
+//             console.log("Killing mainProcess");
+//             mainProcess.kill();
+//             mainProcess = null;
 
-            postScript = fork("dist/postScript.js");
-            instanceManager.setStatus("offline");
+//             postScript = fork("dist/postScript.js");
+//             instanceManager.setStatus("offline");
 
-            // postScript.on("message", onPostScriptMessage);
-            // postScript.on("spawn", onPostScriptMessage);
-        }
-    });
-    await instanceManager.addInstance();
-
-
-    mainProcess = fork("dist/index.js");
-    // mainProcess.on("message", onMainProcessMessage);
-    // mainProcess.on("spawn", onMainProcessMessage);
-
-    // function onMainProcessMessage() {
-    //     // console.log("setting status to online")
-    //     instanceManager.setStatus("online");
-    // }
-
-    // function onPostScriptMessage() {
-    //     // console.log("setting status to offline")
-    //     instanceManager.setStatus("offline");
-    // }
+//             // postScript.on("message", onPostScriptMessage);
+//             // postScript.on("spawn", onPostScriptMessage);
+//         }
+//     });
+//     await instanceManager.addInstance();
 
 
+//     mainProcess = fork("dist/index.js");
+//     // mainProcess.on("message", onMainProcessMessage);
+//     // mainProcess.on("spawn", onMainProcessMessage);
 
-    process.on("exit", () => {
-        console.log("ON EXIT EVENT FIRED")
-    })
+//     // function onMainProcessMessage() {
+//     //     // console.log("setting status to online")
+//     //     instanceManager.setStatus("online");
+//     // }
 
-    process.on("SIGTERM ", () => {
-        console.log("ON SIGTERM EVENT FIRED")
-    })
+//     // function onPostScriptMessage() {
+//     //     // console.log("setting status to offline")
+//     //     instanceManager.setStatus("offline");
+//     // }
 
-    process.on('uncaughtException', (error) => {
-        console.error("here is the error stack", error.stack);
-        console.log("uncaughtException happened", error);
-    });
 
-})();
+
+//     process.on("exit", () => {
+//         console.log("ON EXIT EVENT FIRED")
+//     })
+
+//     process.on("SIGTERM ", () => {
+//         console.log("ON SIGTERM EVENT FIRED")
+//     })
+
+//     process.on('uncaughtException', (error) => {
+//         console.error("here is the error stack", error.stack);
+//         console.log("uncaughtException happened", error);
+//     });
+
+// })();
